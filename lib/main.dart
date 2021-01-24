db61a2369627374811a68a39ec46192fd922ffa9
Shriyash Jalukar <shriyash1111@gmail.com>
2021-01-24 10:05:34 -0500

    work in progress: listview builder

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     // can be something dynamic
 
     // below, we generate a random wordpair using WordPair from the english words library we imported
-    final wordPair = WordPair.random();
+    // final wordPair = WordPair.random();
 
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.purple[900]),
@@ -28,29 +28,30 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   Widget _buildList() {
+
+    final _randomWordPairs = <WordPair>[];
+
     // a ListView is one of the most common scrolling widgets
     // this is the basic layout, from documentation here:
     // https://api.flutter.dev/flutter/widgets/ListView-class.html
-    return ListView(
-      padding: const EdgeInsets.all(8),
-      children: <Widget>[
-        Container(
-          height: 50,
-          color: Colors.amber[600],
-          child: const Center(child: Text('Entry A')),
-        ),
-        Container(
-          height: 50,
-          color: Colors.amber[500],
-          child: const Center(child: Text('Entry B')),
-        ),
-        Container(
-          height: 50,
-          color: Colors.amber[100],
-          child: const Center(child: Text('Entry C')),
-        ),
-      ],
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (context, item) {
+        if(item.isOdd) return Divider();
+
+        final index = item ~/ 2;
+
+        if(index >= _randomWordPairs.length) {
+          _randomWordPairs.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_randomWordPairs[index]); 
+      },
     );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(title: Text('Hello'));
   }
 
   Widget build(BuildContext context) {
