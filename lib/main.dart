07e95c24732342c442a85899ed386c2fe3675304
Shriyash Jalukar <shriyash1111@gmail.com>
2021-01-24 10:07:53 -0500

    adding wordpairs rather than placeholder text

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,8 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
-    return ListTile(title: Text('Hello'));
+    return ListTile(title: Text(pair.asPascalCase, style:
+      TextStyle(fontSize: 18.0)));
   }
 
   Widget build(BuildContext context) {
